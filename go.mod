601bda8c19518c37a4e1c70396afb9f878ae46f4
Alessandro <alessandro.guglielmin@jobtome.com>
2021-06-09 15:02:49 +0200

    proto files

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/override80/grpc-gateway-hello-world
 go 1.16
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20210426193834-eac7f76ac494 // indirect
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 )
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 
 	helloworldpb "github.com/override80/grpc-gateway-hello-world/proto/helloworld"
 )
 
-type server struct{}
+type server struct{
+	helloworldpb.UnimplementedGreeterServer
+}
 
 func NewServer() *server {
 	return &server{}
@@ -31,7 +35,36 @@ func main() {
 	s := grpc.NewServer()
 	// Attach the Greeter service to the server
 	helloworldpb.RegisterGreeterServer(s, &server{})
-	// Serve gRPC Server
+	// Serve gRPC server
 	log.Println("Serving gRPC on 0.0.0.0:8080")
-	log.Fatal(s.Serve(lis))
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = helloworldpb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
